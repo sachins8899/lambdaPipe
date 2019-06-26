394f82349608734916e2e63edf75c4c9e8226f18
sachins8899 <44035555+sachins8899@users.noreply.github.com>
2019-06-26 10:02:18 +0530

    normal first code commit

---
+++ src/main/java/com/test/pipe/pipe/LambdaFunctionHandler.java
@@ -0,0 +1,15 @@
+package com.test.pipe.pipe;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        return "Hello from Lambda!";
+    }
+
+}
